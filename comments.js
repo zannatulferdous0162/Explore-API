8a74ba017c560435f8b2fd4ad5bd178b3f13c035
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-02-27 18:36:28 +0600

    add some fetch

---
+++ comments.js
@@ -0,0 +1,18 @@
+const loadComments = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error happend', error))
+}
+
+const loadComments2 = async() => {
+    try{
+        const  res = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await  res.json();
+    console.log(data);
+    }
+    catch(error){
+        console.log('data load error')
+    }
+}
+
