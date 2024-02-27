08ac534172e8e95f41e7141a97688408de0dd054
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-02-27 07:35:01 +0600

    add some fetch

--- fetch.js
+++ fetch.js
@@ -1,6 +1,13 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => console.log(response)) //  .JSON is not similar  bt close to JSON.parse
-      // .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => console.log(response)) //  .JSON is not similar  bt close to JSON.parse
+//       .then(json => console.log(json))
 
 
-fetch('')      
+
+function loadData(){
+      
+      const url = 'https://jsonplaceholder.typicode.com/todos/1';
+      fetch(url)
+.then(Response => Response.json())
+.then(json => console.log(json))
+}
